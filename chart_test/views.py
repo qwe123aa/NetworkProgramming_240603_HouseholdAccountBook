17f1bc26960f9c51f314fd891f76a9fbd2052099
thdud__ <sylim1114y@gmail.com>
2024-06-10 11:42:26 +0900

    chart_test 완료(label, data, backgroundColor)

--- chart_test/views.py
+++ chart_test/views.py
@@ -1,5 +1,19 @@
+import json
+
 from django.shortcuts import render
 
 def show_chart(request):
-    context = {}
+    with open('data/data.json', 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    name = [item['name'] for item in json_data]
+    price = [item['price'] for item in json_data]
+    ctgColor = [item['ctgColor'] for item in json_data]
+
+    context = {
+        'name' : name,
+        'price' : price,
+        'ctgColor' : ctgColor
+    }
+
     return render(request, 'chart_test/chart_view.html', context=context)
