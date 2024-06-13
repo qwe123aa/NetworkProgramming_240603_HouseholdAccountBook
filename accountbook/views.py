1d034e7cef5bc45b35c7e340dd3d64d00c7e0887
thdud__ <sylim1114y@gmail.com>
2024-06-13 15:10:04 +0900

    views categoryListView; templates category_list.html; urls accountbook:category_list

--- accountbook/views.py
+++ accountbook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from accountbook.models import Category
+
+
+class CategoryListView(ListView):
+    model = Category    #category 테이블의 모든 category를 가져와 category_list라는 context에 담아 category_list.html에 넘김
